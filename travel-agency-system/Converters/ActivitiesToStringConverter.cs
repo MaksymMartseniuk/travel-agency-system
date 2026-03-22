c1a75617b57fa9e02111a8fbc1c41ef974f9b72a
MaksymMartseniuk <marcenukm995@gmail.com>
2026-03-22 10:33:05 +0200

    feat: add convertor for activities

---
+++ travel-agency-system/Converters/ActivitiesToStringConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using travel_agency_system.Models;
+
+namespace travel_agency_system.Converters
+{
+    public class ActivitiesToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IEnumerable<TourActivity> activities)
+            {
+                string result = string.Join(", ", activities);
+                return string.IsNullOrEmpty(result) ? "None" : result;
+            }
+            return "None";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
